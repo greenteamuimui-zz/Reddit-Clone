234b23134c45a1052979d3fa50378764bdb8a7cc
Cherry Lam <siuding0601@gmail.com>
2017-06-26 18:11:12 -0700

    Finish recursive call on baby comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,5 +12,8 @@ class Post < ApplicationRecord
   has_many :post_subs
   has_many :subs, through: :post_subs, source: :sub
 
-  
+  def top_level_comments
+    self.comments.where(parent_comment_id:nil)
+  end
+
 end
